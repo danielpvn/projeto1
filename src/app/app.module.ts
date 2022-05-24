ac6c098de371842cccc5fdee23a06d914592692b
Daniel Pereira Vasconcelos nunes <daniel.pvn@hotmail.com>
2022-05-24 09:30:44 -0300

    angular1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelPrincipalComponent } from './painel-principal/painel-principal.component';
+import { CadastroTarefasComponent } from './cadastro-tarefas/cadastro-tarefas.component';
+import { ConsultaTarefasComponent } from './consulta-tarefas/consulta-tarefas.component';
+
+import { Routes} from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: 'painel-principal',component: PainelPrincipalComponent
+  },
+  {
+    path: 'cadastro-tarefas',component: CadastroTarefasComponent
+  },
+  {
+    path: 'consulta-tarefas',component: ConsultaTarefasComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelPrincipalComponent,
+    CadastroTarefasComponent,
+    ConsultaTarefasComponent
   ],
   imports: [
     BrowserModule,
